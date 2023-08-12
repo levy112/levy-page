0b6b6673b306ee1f7035e4f3e0f8a355a9c54b64
levy112 <levyjoanisanchezrivera@gmail.com>
2023-08-12 06:13:37 +0200

    version1.5

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
+        document.getElementById("python").classList.add("barra-progreso0");
         document.getElementById("html").classList.add("barra-progreso1");
         document.getElementById("js").classList.add("barra-progreso2");
         document.getElementById("bd").classList.add("barra-progreso3");
